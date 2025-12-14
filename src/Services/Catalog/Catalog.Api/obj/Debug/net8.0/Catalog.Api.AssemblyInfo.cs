f0424b811805a6ba449ef9bc561c1e75c5dbf077
AhmedFathiMusfer <ahmdfthymsfra@gmail.com>
2025-12-15 01:59:55 +0300

    Add gRPC dependencies and project configuration for Discount.Grpc service
    - Created `Discount.Grpc.csproj.nuget.g.targets` to include gRPC tools.
    - Added `project.assets.json` to manage package dependencies for .NET 8.0.
    - Generated `project.nuget.cache` to track expected package files and ensure successful restoration.

--- src/Services/Catalog/Catalog.Api/obj/Debug/net8.0/Catalog.Api.AssemblyInfo.cs
+++ src/Services/Catalog/Catalog.Api/obj/Debug/net8.0/Catalog.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e78788654467ec7fdcd6ce4fa986874ac096fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1a96119ef8f96daab2b864c11bf2bbeb65271d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
