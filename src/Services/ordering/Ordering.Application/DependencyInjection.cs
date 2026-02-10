3205d71096380ecdbe3ea4341f5c98a688992d3a
AhmedFathiMusfer <ahmdfthymsfra@gmail.com>
2026-02-11 00:32:40 +0300

    feat: Initialize Ordering microservice structure with Application, Domain, and Infrastructure layers
    - Added Ordering.Application project with DependencyInjection class for service registration.
    - Created Ordering.Domain project with a sample Class1.
    - Established Ordering.Infrastructure project with DependencyInjection class for infrastructure services.
    - Configured project files for .NET 8.0 and enabled nullable reference types.
    - Included necessary NuGet package configurations and project asset files.

---
+++ src/Services/ordering/Ordering.Application/DependencyInjection.cs
@@ -0,0 +1,10 @@
+
+namespace Ordering.Application
+{
+    public class DependencyInjection
+    {
+        static IServesCollection AddApplicationSevcies(this IServesCollection service)  {
+               return service;
+        }
+    }
+}
---
+++ src/Services/ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,11 @@
+
+
+namespace Ordering.Infrastructure
+{
+    public class DependencyInjection
+    {
+         static IServesCollection AddInfrastructreSevcies(this IServesCollection service)  {
+               return service;
+        }
+    }
+}
